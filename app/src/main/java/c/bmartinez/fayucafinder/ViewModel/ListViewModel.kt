df80cffa0ce34434c779cb9be3f590c4b481ae2c
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2019-10-13 21:57:20 -0700

    Setting up the maps fragment

--- app/src/main/java/c/bmartinez/fayucafinder/ViewModel/ListViewModel.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/ViewModel/ListViewModel.kt
@@ -1,13 +1,13 @@
-package c.bmartinez.fayucafinder.ui.notifications
+package c.bmartinez.fayucafinder.ViewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ListViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/c/bmartinez/fayucafinder/ViewModel/MapViewModel.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/ViewModel/MapViewModel.kt
@@ -1,10 +1,10 @@
-package c.bmartinez.fayucafinder.ui.home
+package c.bmartinez.fayucafinder.ViewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class MapViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
