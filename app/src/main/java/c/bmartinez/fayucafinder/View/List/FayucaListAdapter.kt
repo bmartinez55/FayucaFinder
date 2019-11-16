ef83fbbe3617b795d60a5dcea0055c6563fd9944
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2019-11-15 22:44:20 -0800

    Next step is to incorporate Data binding to the adapter

--- app/src/main/java/c/bmartinez/fayucafinder/View/List/FayucaListAdapter.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/View/List/FayucaListAdapter.kt
@@ -1,10 +1,38 @@
 package c.bmartinez.fayucafinder.View.List
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.BinderThread
 import androidx.recyclerview.widget.RecyclerView
 import c.bmartinez.fayucafinder.Model.fayucaDao
+import c.bmartinez.fayucafinder.R
 
-class FayucaListAdapter: RecyclerView.Adapter<FayucaListAdapter.FayucaViewHolder> {
-    private val fayucaList: ArrayList<fayucaDao>
-    private lateinit var mContext: Context
+class FayucaListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private var mFayucaList: ArrayList<fayucaDao>
+    private var mContext: Context
+
+    constructor(context: Context, fayucas: ArrayList<fayucaDao>){
+        mContext = context
+        this.mFayucaList = fayucas
+    }
+
+    override  fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        return mFayucaList.size
+    }
+}
+
+class FayucaViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    @BinderThread
 }
