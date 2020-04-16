3671ac9dac48cfb09cdc5640dc9d847f91dee85e
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-04-15 19:01:53 -0700

    Fixed DAO. Next need to fix the model database

--- app/src/main/java/c/bmartinez/fayucafinder/Model/TrucksDao.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/Model/TrucksDao.kt
@@ -1,14 +1,13 @@
 package c.bmartinez.fayucafinder.Model
 
+import com.google.firebase.database.IgnoreExtraProperties
 import com.google.firebase.firestore.GeoPoint
-import java.io.Serializable
 
+@IgnoreExtraProperties
 data class TrucksDao(var idName: String? = "",
     var name: String? = null,
     var description: String? = null,
-    var location: GeoPoint? = null) : Serializable{
-
-    constructor(): this("","","",null)
+    var location: GeoPoint? = null){
 
     fun getTruckName(): String? {
         return name
