8741f04a3b9ee1d04e3dafeafe3e0a4645fc0e56
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-04-20 20:39:19 -0700

    Repo and ViewModel fixed. Next is the View

--- app/src/main/java/c/bmartinez/fayucafinder/Model/Database/FireRepository.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/Model/Database/FireRepository.kt
@@ -2,48 +2,37 @@ package c.bmartinez.fayucafinder.Model.Database
 
 import android.util.Log
 import com.google.firebase.database.*
-import androidx.lifecycle.LiveData
 import c.bmartinez.fayucafinder.Model.TrucksDao
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-//: LiveData<DataSnapshot>()
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 class FireRepository{
+    private val database = FirebaseFirestore.getInstance()
+//    private val myReference = database.reference
+//    private val truckNode = myReference.child("truckOwners")
+//    private lateinit var onTruck: ((TrucksDao?) -> Unit)
+//    private lateinit var truckId: String
 
-    private val database = FirebaseDatabase.getInstance()
-    private val myReference = database.reference
-    private val truckNode = myReference.child("truckOwners")
-    private lateinit var onTruck: ((TrucksDao?) -> Unit)
-    private lateinit var truckId: String
-
-    private  val valueEventListener = object : ValueEventListener{
-        override fun onDataChange(p0: DataSnapshot) {
-            val truckData = p0.child(truckId).getValue(TrucksDao::class.java)
-            onTruck(truckData)
-            closeListener()
-        }
-        override fun onCancelled(p0: DatabaseError) {
-            onTruck(null)
-            closeListener()
-        }
-
-    }
-
-    fun getTruck(id: String, onTruck: ((TrucksDao?) -> Unit)){
-        
-    }
-
-    private fun closeListener(){
-        truckNode.removeEventListener(valueEventListener)
-    }
-
-//    override fun onActive() {
-//        Log.d("FirebaseRepository ", "Location: onActive")
-//        truckNode.addValueEventListener(valueEventListener)
-//    }
+//    private  val valueEventListener = object : ValueEventListener{
+//        override fun onDataChange(p0: DataSnapshot) {
+//            val truckData = p0.child(truckId).getValue(TrucksDao::class.java)
+//            onTruck(truckData)
+//            closeListener()
+//        }
+//        override fun onCancelled(p0: DatabaseError) {
+//            onTruck(null)
+//            closeListener()
+//        }
 //
-//    override fun onInactive() {
-//        Log.d("FirebaseRepository ", "Location: onInactive")
+//    }
+
+    fun getTrucks(): CollectionReference {
+        return database.collection("truckOwners")
+    }
+
+//    private fun closeListener(){
 //        truckNode.removeEventListener(valueEventListener)
 //    }
 
