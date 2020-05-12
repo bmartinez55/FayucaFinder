5eb279ed5469c3d06ee5956827d873d9d045da7c
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-05-11 23:10:49 -0700

    Fix AppModule error regarding @Provides

--- app/src/main/java/c/bmartinez/fayucafinder/DataInjection/Module/FirebaseRepositoryModule.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/DataInjection/Module/FirebaseRepositoryModule.kt
@@ -1,6 +1,5 @@
 package c.bmartinez.fayucafinder.DataInjection.Module
 
-import c.bmartinez.fayucafinder.DataInjection.Scope.AppScoped
 import c.bmartinez.fayucafinder.Model.Database.FireRepository
 import dagger.Module
 import dagger.Provides
@@ -8,6 +7,5 @@ import dagger.Provides
 @Module
 class FirebaseRepositoryModule {
     @Provides
-    @AppScoped
     fun provideFirebaseRepository(): FireRepository = FireRepository()
 }
--- app/src/main/java/c/bmartinez/fayucafinder/ViewModel/MapViewModel.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/ViewModel/MapViewModel.kt
@@ -1,29 +1,21 @@
 package c.bmartinez.fayucafinder.ViewModel
 
-import android.util.Log
-import android.view.View
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import c.bmartinez.fayucafinder.Base.BaseViewModel
-import c.bmartinez.fayucafinder.Base.ViewState
-import c.bmartinez.fayucafinder.DataInjection.Scope.AppScoped
 import c.bmartinez.fayucafinder.Model.Database.FireRepository
 import c.bmartinez.fayucafinder.Model.Trucks
-import c.bmartinez.fayucafinder.View.map.MapViewState
-import com.google.firebase.firestore.EventListener
-import kotlinx.coroutines.supervisorScope
 import javax.inject.Inject
 
-@AppScoped
-class MapViewModel @Inject constructor(fireRepo: @JvmSuppressWildcards(true) FireRepository,
-                                       viewState: @JvmSuppressWildcards(true) MapViewState): BaseViewModel<MapViewState>(fireRepo, viewState){
+class MapViewModel @Inject constructor(truckDao: Trucks): ViewModel() {
     val TAG = "MAP_VIEW_MODEL"
 
+    private lateinit var truckRepository: FireRepository
+    private val truckLiveData = MutableLiveData<ArrayList<Trucks>>()
+
     private fun launchMapActivity(){
-        fireRepository.getTrucks()
-        viewState.trucksViewModelLiveDate = fireRepository.trucksRepoLiveData
-        updateUi()
+        truckRepository.getTrucks()
     }
+
+    fun getTruckData() = truckLiveData
 
 }
