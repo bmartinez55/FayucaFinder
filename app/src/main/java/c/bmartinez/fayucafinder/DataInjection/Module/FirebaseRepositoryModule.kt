64b0bb773ab809faee3b4dedcd1a247f13cc1599
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-05-16 16:46:20 -0700

    Pause and Save Point

--- app/src/main/java/c/bmartinez/fayucafinder/DataInjection/Module/FirebaseRepositoryModule.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/DataInjection/Module/FirebaseRepositoryModule.kt
@@ -3,9 +3,11 @@ package c.bmartinez.fayucafinder.DataInjection.Module
 import c.bmartinez.fayucafinder.Model.Database.FireRepository
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class FirebaseRepositoryModule {
     @Provides
+    @Singleton
     fun provideFirebaseRepository(): FireRepository = FireRepository()
 }
