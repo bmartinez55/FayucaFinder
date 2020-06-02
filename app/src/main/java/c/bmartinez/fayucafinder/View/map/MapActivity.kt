8f3a221198244ef78bfb4d1d1159cc2ea5bc9b97
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-06-02 12:07:44 -0700

    Fixed dagger dependecies error as well as app boot crash. Next, is to pass the data and create markers.

--- app/src/main/java/c/bmartinez/fayucafinder/View/map/MapActivity.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/View/map/MapActivity.kt
@@ -2,15 +2,18 @@ package c.bmartinez.fayucafinder.View.map
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import c.bmartinez.fayucafinder.DataInjection.Components.AppComponent
+import c.bmartinez.fayucafinder.DataInjection.ViewModelFactory
 import c.bmartinez.fayucafinder.R
 import com.google.firebase.FirebaseApp
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 //HasSupportFragmentInjector
 class MapActivity :DaggerAppCompatActivity()  {
@@ -34,8 +37,14 @@ class MapActivity :DaggerAppCompatActivity()  {
 
         noActionBar()
         setupBottomNavBar(navController)
-        setUpActivity()
+        //setUpActivity()
+        testFragment()
     }
+
+    private fun testFragment(){
+        supportFragmentManager.beginTransaction().replace(R.id.main_container, MapsFragment()).commit()
+    }
+
     private fun setupBottomNavBar(navController: NavController){
         nav_view?.let {
             NavigationUI.setupWithNavController(it, navController)
@@ -47,15 +56,15 @@ class MapActivity :DaggerAppCompatActivity()  {
         actionBar?.hide()
     }
 
-    private fun setUpActivity(){
-        var fragment = supportFragmentManager.findFragmentById(R.id.map)
+    //private fun setUpActivity(){
+        //var fragment = supportFragmentManager.findFragmentById(R.id.map)
         //fragment = supportFragmentManager.beginTransaction().replace(R.id.main_container, R.id.map )
 
-        if(fragment == null){
+        //if(fragment == null){
             //fragment = mapFragment
             //ActivityUtilities.addFragmentToActivity(supportFragmentManager,fragment,
             //R.id.map)
-        }
-    }
+        //}
+    //}
 }
 
