db5b28f8d0faf473c07187c9898707a239d9270f
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-06-04 00:18:29 -0700

    Fix read data from Firestore with MVVM. Cause of error was utilizing firebase database coding method instead of the firestore method.

--- app/src/main/java/c/bmartinez/fayucafinder/Model/Database/FireRepository.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/Model/Database/FireRepository.kt
@@ -1,37 +1,15 @@
 package c.bmartinez.fayucafinder.Model.Database
 
-import androidx.lifecycle.MutableLiveData
-import c.bmartinez.fayucafinder.Model.Trucks
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.GeoPoint
 import javax.inject.Singleton
 
 @Singleton
 class FireRepository {
     private val database = FirebaseFirestore.getInstance()
-    val trucksRepoLiveData = MutableLiveData<ArrayList<Trucks>>()
+    //val trucksRepoLiveData = MutableLiveData<ArrayList<Trucks>>()
 
-    fun callData(): CollectionReference{
+    fun callData(): CollectionReference {
         return database.collection("truckOwners")
     }
-
-    fun getTrucks() {
-        database.collection("truckOwners").addSnapshotListener{ snapShot, exception ->
-            if(exception != null){
-                exception.printStackTrace()
-                return@addSnapshotListener
-            }
-
-            val trucks = snapShot?.documents?.map{
-                Trucks(
-                    it["Name"] as String,
-                    it["description"] as String,
-                    (it["location"]) as GeoPoint
-                )
-            }
-            trucks?.let { trucksRepoLiveData.postValue(trucks as ArrayList<Trucks>?) }
-        }
-    }
-
 }
