e60584b4a9eb252472cc7357596ada286c510f24
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-06-05 16:41:21 -0700

    Added neccessary files for future features. Finished set up of the RecyclerView layout

--- app/src/main/java/c/bmartinez/fayucafinder/Model/Database/FireRepository.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/Model/Database/FireRepository.kt
@@ -2,14 +2,21 @@ package c.bmartinez.fayucafinder.Model.Database
 
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import javax.inject.Singleton
 
 @Singleton
 class FireRepository {
     private val database = FirebaseFirestore.getInstance()
-    //val trucksRepoLiveData = MutableLiveData<ArrayList<Trucks>>()
+    //lateinit var  storage: FirebaseStorage
+    //private val storageRef: StorageReference? = storage.reference
 
     fun callData(): CollectionReference {
         return database.collection("truckOwners")
     }
+
+//    fun callStorage(): StorageReference{
+//        return storageRef!!.child("testImages")
+//    }
 }
