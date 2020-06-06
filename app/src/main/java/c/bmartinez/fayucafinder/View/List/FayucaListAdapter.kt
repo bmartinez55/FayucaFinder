c5021bc13866b26e3b0a3f06dd521b031188a0fc
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-06-05 17:12:20 -0700

    Pause Point: Finish tomorrow the ListAdapter

--- app/src/main/java/c/bmartinez/fayucafinder/View/List/FayucaListAdapter.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/View/List/FayucaListAdapter.kt
@@ -1,29 +1,45 @@
 package c.bmartinez.fayucafinder.View.List
 
-//class FayucaListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    /*private var mFayucaList: ArrayList<fayucaDao>
-    private var mContext: Context
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import c.bmartinez.fayucafinder.Model.Trucks
+import c.bmartinez.fayucafinder.R
 
-    constructor(context: Context, fayucas: ArrayList<fayucaDao>){
-        mContext = context
-        this.mFayucaList = fayucas
-    }
+class FayucaListAdapter(context: Context, fayucas: ArrayList<Trucks>) : RecyclerView.Adapter<ViewHolder>() {
+    private var mFayucaList: ArrayList<Trucks> = fayucas
+    private var mContext: Context = context
 
-    override  fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return RecyclerView.ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.fayuca_list_item,parent,false))
+    override  fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val rootView: View = LayoutInflater.from(mContext).inflate(R.layout.fayuca_list_item,parent,false)
+        return RecyclerViewViewHolder(rootView)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-       holder?.fayucaNameText?.text = mFayucaList.get(position)
+       val trucks: Trucks = mFayucaList.get(position)
+        val viewHolder: RecyclerViewViewHolder = holder as RecyclerViewViewHolder
+
+        viewHolder.textViewName.setText(trucks.name)
+
     }
 
     override fun getItemCount(): Int {
         return mFayucaList.size
     }
-}
 
-class FayucaViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-    val vehicleName = view.fayucaNameText
-    val vehicleAddress = view.fayucaAddressText
-    val vehicleDistance = view.distanceTextView*/
-//}
+    class RecyclerViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var imageView: ImageView
+        lateinit var textViewName: TextView
+
+        fun RecyclerViewViewHolder(itemView: View){
+            imageView = itemView.findViewById(R.id.imageView)
+            textViewName = itemView.findViewById(R.id.truck_title)
+        }
+
+    }
+}
