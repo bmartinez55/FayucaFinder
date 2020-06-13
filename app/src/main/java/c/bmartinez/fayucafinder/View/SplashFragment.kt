0f1d98eaa6182c41bda6b80aea92eaa6f76358b1
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-06-12 22:40:13 -0700

    Added files for Welcome Screen. Progess Save Point: Finish setting up the XML for the welcome page

--- app/src/main/java/c/bmartinez/fayucafinder/View/SplashFragment.kt
+++
@@ -1,7 +0,0 @@
-package c.bmartinez.fayucafinder.View
-
-import androidx.fragment.app.Fragment
-
-class SplashFragment: Fragment() {
-
-}
---
+++ app/src/main/java/c/bmartinez/fayucafinder/View/welcome/WelcomeActivity.kt
@@ -0,0 +1,10 @@
+package c.bmartinez.fayucafinder.View.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class WelcomeActivity : AppCompatActivity(){
+
+
+}
+
