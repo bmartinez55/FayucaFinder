5f3dec6323a2f23c7a0c14d850ae4f48c9f7adc9
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-06-26 23:41:25 -0700

    Save Point: Research fragment navigation thoroughly.

--- app/src/main/java/c/bmartinez/fayucafinder/View/login/LoginActivity.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/View/login/LoginActivity.kt
@@ -1,10 +1,24 @@
 package c.bmartinez.fayucafinder.View.login
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import c.bmartinez.fayucafinder.R
+import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_holder.view.*
 
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : DaggerAppCompatActivity(){
+    private lateinit var nav: NavController
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_holder)
+
+        nav = Navigation.findNavController(this, R.id.navHostFragment)
+    }
 
 }
 
