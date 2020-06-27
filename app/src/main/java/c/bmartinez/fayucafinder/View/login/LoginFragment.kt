b9ce23a4668ebbd7ae1822ad96aeeb5fdb3401cf
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-06-26 23:11:20 -0700

    Added register fragment to the nav graph and added tools package to the fragment to show a preview of the UI while viewing the nav graph.

--- app/src/main/java/c/bmartinez/fayucafinder/View/login/LoginFragment.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/View/login/LoginFragment.kt
@@ -9,7 +9,7 @@ import c.bmartinez.fayucafinder.R
 import dagger.android.support.DaggerFragment
 
 class LoginFragment: DaggerFragment() {
-    
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
