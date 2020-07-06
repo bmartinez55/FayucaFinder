a1d9b830d8769d694f235fbdf1c4fb675b1bde89
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-07-06 15:31:50 -0700

    Save Point: Integrate Auth structure for MVVM and Coroutines

--- app/src/main/java/c/bmartinez/fayucafinder/View/login/LoginViewModel.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/View/login/LoginViewModel.kt
@@ -6,10 +6,15 @@ import androidx.lifecycle.ViewModel
 import c.bmartinez.fayucafinder.Model.Database.FireRepository
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(val fireRepository: FireRepository): ViewModel() {
     val TAG = "LOGIN_VIEW_MODEL"
 
+    val user = fireRepository.user()
 
+    suspend fun signInWithEmailPassword(email: String, password: String){
+        fireRepository.login(email,password)
+    }
 }
