42c0d610f5eab605be69801e128e14ae96e43954
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2020-07-09 21:21:50 -0700

    Implemented the User DAO

--- app/src/main/java/c/bmartinez/fayucafinder/Model/User.kt
+++ app/src/main/java/c/bmartinez/fayucafinder/Model/User.kt
@@ -1,4 +1,13 @@
 package c.bmartinez.fayucafinder.Model
 
-class User {
+import c.bmartinez.fayucafinder.Model.Database.FirebaseAuth
+
+class User constructor(private val firebaseSource: FirebaseAuth) {
+    suspend fun login(email: String, password: String) = firebaseSource.login(email,password)
+
+    suspend fun register(email: String, password: String) = firebaseSource.register(email, password)
+
+    suspend fun currentUser() = firebaseSource.currentUser()
+
+    suspend fun logout() = firebaseSource.logout()
 }
